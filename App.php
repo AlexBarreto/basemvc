01b7ee795d9bc13cc5b80535c2e16cabf5bdd303
AlexBarreto <alexbarreto2020@hotmail.com>
2021-12-14 14:40:09 -0300

    Configuração autoload

---
+++ App.php
@@ -0,0 +1,30 @@
+<?php
+
+class App{
+
+/*
+
+@return type
+Responsável em carregar a classe controladora
+das paginas que estamos visualizando;
+
+*/
+
+  public function executar(){
+    $url = isset($_GET['url'])? explode('/',$_GET['url'])[0] : 'Home';
+    $url = ucfirst($url);
+    $url.="Controller";
+
+    if(file_exists("Controllers/{$url}.php")){
+      $className = "Controllers\\{$url}";
+      $controler = new $className();
+      $controler->executar();
+    }else{
+      die('Não existe esse controller!');
+    }
+  }
+}
+
+
+
+?> 
